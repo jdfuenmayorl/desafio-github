2cb1a7e78130517e933955ab3b2b13c2c19f7bb4
jdfuenmayorl <josedfuenmayorl@gmail.com>
2018-07-14 12:22:18 -0400

    Se agrega ciclo para solicitar digitos

--- clase.java
+++ clase.java
@@ -6,5 +6,9 @@ public class Desafio {
       Scanner in = new Scanner();
       int[] ar = int[3];
 
+      for (int i = 0; i < ar.length; i++){
+        System.out.print("Ingrese valor de posicion " + i + " :");
+        int[i] = in.nextInt();
+      }
   }
 }
